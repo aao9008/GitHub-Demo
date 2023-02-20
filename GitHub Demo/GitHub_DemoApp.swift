f68f44ce22fdae165f2b6b37a185f2f8e9ee7df8
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-02-20 01:53:20 -0500

    Initial Commit

---
+++ GitHub Demo/GitHub_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_DemoApp.swift
+//  GitHub Demo
+//
+//  Created by Alfredo Ormeno Zuniga on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
